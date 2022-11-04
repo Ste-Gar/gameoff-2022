4f333427075764dd05d904f85294c1f77776f755
Ste-Gar <ste.gargiulo@gmail.com>
2022-11-04 16:43:16 +0100

    Added gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,34 +4,82 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    const string HORIZONTAL_AXIS = "Horizontal";
+    const string VERTICAL_AXIS = "Vertical";
+
     Rigidbody rb;
     float forwardInput;
     float lateralInput;
-    const string HORIZONTAL_AXIS = "Horizontal";
-    const string VERTICAL_AXIS = "Vertical";
+    Vector3 movementDirection;
+    CharacterController charController;
+    Transform mainCam;
+    float turnSmoothVelocity;
+    [SerializeField] float turnSmoothTime = .1f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float gravityScale = 10;
 
     [SerializeField] float moveSpeed = 5;
     [SerializeField] ForceMode forceMode = ForceMode.Acceleration;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        charController = GetComponent<CharacterController>();
+        mainCam = Camera.main.transform;
     }
 
-    void Start()
+    private void Update()
     {
-        
+        float forwardInput = Input.GetAxisRaw(HORIZONTAL_AXIS);
+        float lateralInput = Input.GetAxisRaw(VERTICAL_AXIS);
+        Vector3 movement = new Vector3(forwardInput, 0, lateralInput);//.normalized;
+        Vector3 moveDirection;
+
+        if (movement.magnitude > 0)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + mainCam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            Quaternion rotation = Quaternion.Euler(0, angle, 0);
+            transform.rotation = rotation;
+
+            moveDirection = rotation * Vector3.forward;
+        }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
+
+        moveDirection.y += gravity;
+        charController.Move(moveDirection * moveSpeed * Time.deltaTime);
     }
 
-    void Update()
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        forwardInput = Input.GetAxis(HORIZONTAL_AXIS);
-        lateralInput = Input.GetAxis(VERTICAL_AXIS);
+        Debug.Log(hit.gameObject.name);
     }
 
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(forwardInput, 0, lateralInput) * moveSpeed;
-        rb.AddForce(movement, forceMode);
-    }
+    //private void Awake()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //    mainCam = Camera.main.transform;
+    //}
+
+    //void Update()
+    //{
+    //    forwardInput = Input.GetAxis(HORIZONTAL_AXIS);
+    //    lateralInput = Input.GetAxis(VERTICAL_AXIS);
+    //    Vector3 movementInput = new Vector3(forwardInput, 0, lateralInput);
+
+    //    float targetAngle = Mathf.Atan2(movementInput.x, movementInput.z) * Mathf.Rad2Deg + mainCam.eulerAngles.y;
+    //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+    //    Quaternion rotation = Quaternion.Euler(0, angle, 0);
+    //    transform.rotation = rotation;
+    //    movementDirection = rotation * Vector3.forward;
+    //    Debug.Log(movementDirection);
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    Vector3 movement = new Vector3(forwardInput, 0, lateralInput) * moveSpeed;
+    //    rb.AddForce(movement, forceMode);
+    //}
 }
