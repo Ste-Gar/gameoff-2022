c2f5df095893e9d31b526d9095289fc4c4daabb1
Ste-Gar <ste.gargiulo@gmail.com>
2022-11-07 15:02:16 +0100

    Basic enable ragdoll + camera fix
    camera now follows hip bone

--- Assets/Scripts/RagdollManager.cs
+++ Assets/Scripts/RagdollManager.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class RagdollManager : MonoBehaviour
 {
+    enum RagdollState
+    {
+        disabled,
+        enabled
+    }
+
+    RagdollState state = RagdollState.disabled;
     private Rigidbody[] ragdollRigidbodies;
+
+    CharacterController characterController;
+    PlayerMovement playerMovement;
+    [SerializeField] Animator animator;
+
     private void Awake()
     {
         ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            EnableRagdoll();
+    }
+
+    private void EnableRagdoll()
+    {
+        state = RagdollState.enabled;
+
+        characterController.enabled = false;
+        playerMovement.enabled = false;
+        animator.enabled = false;
+
+        foreach (Rigidbody rb in ragdollRigidbodies)
+        {
+            rb.isKinematic = false;
+        }
     }
 }
