ca2063d36a19a2a0e9eebf5d7abed3cc92e9209e
Ste-Gar <ste.gargiulo@gmail.com>
2022-11-07 17:54:25 +0100

    shitty collisions

--- Assets/Scripts/RagdollManager.cs
+++ Assets/Scripts/RagdollManager.cs
@@ -13,35 +13,88 @@ public class RagdollManager : MonoBehaviour
     RagdollState state = RagdollState.disabled;
     private Rigidbody[] ragdollRigidbodies;
 
+    CapsuleCollider playerCollider;
     CharacterController characterController;
     PlayerMovement playerMovement;
     [SerializeField] Animator animator;
+
+    [SerializeField] float collisionForceMulti = 100;
 
     private void Awake()
     {
         ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
         characterController = GetComponent<CharacterController>();
         playerMovement = GetComponent<PlayerMovement>();
+        playerCollider = GetComponent<CapsuleCollider>();
     }
 
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Z))
-            EnableRagdoll();
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //    EnableRagdoll(new Vector3(0, 10000, 0));
     }
 
-    private void EnableRagdoll()
+    private void EnableRagdoll(Vector3 force)
     {
         state = RagdollState.enabled;
 
         characterController.enabled = false;
         playerMovement.enabled = false;
         animator.enabled = false;
+        playerCollider.enabled = false;
 
         foreach (Rigidbody rb in ragdollRigidbodies)
         {
             rb.isKinematic = false;
+            //rb.AddExplosionForce(300f, hitPoint, .1f, 0, ForceMode.Impulse);
+            rb.AddForce(force);
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Vehicle"))
+        {
+            Debug.Log("hit");
+            Vector3 hitDirection = (other.transform.position - transform.position).normalized;
+            Vector3 vehicleVelocity = other.attachedRigidbody.velocity;
+            Vector3 playerVelocity = characterController.velocity;
+            Vector3 relativeVelocity = vehicleVelocity + playerVelocity + Vector3.up * 10;
+
+            Vector3 hitForce = Vector3.Scale(relativeVelocity, hitDirection) * collisionForceMulti;
+
+            EnableRagdoll(hitForce);
+        }    
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    //Debug.Log(collision.gameObject.name);
+    //    if (collision.gameObject.CompareTag("Vehicle"))
+    //    {
+    //        Vector3 relativeVelocity = collision.relativeVelocity;
+
+
+    //        EnableRagdoll(collision.relativeVelocity);
+    //    }
+    //}
+    //private void OnControllerColliderHit(ControllerColliderHit hit)
+    //{
+    //    Debug.Log(hit.gameObject.name);
+    //    if (hit.gameObject.CompareTag("Vehicle"))
+    //    {
+    //        Vector3 vehicleVelocity = hit.rigidbody.velocity;
+    //        Vector3 characterVelocity = hit.controller.velocity;
+    //        Vector3 hitVelocity = vehicleVelocity + characterVelocity;
+
+    //        //Vector3 hitPoint = hit.point;
+    //        Vector3 hitDirection = (hit.point - transform.position).normalized;
+    //        Vector3 hitForce = Vector3.Scale(hitVelocity, hitDirection) * 30000;
+
+
+
+    //        EnableRagdoll(hitForce);
+    //    }
+    //}
 }
